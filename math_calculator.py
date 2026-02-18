253770db909430a189379acdef80b681a216b5cc
David Catinac <catinacjts7@gmail.com>
2026-02-18 01:05:03 -0500

    Added code

--- math_calculator.py
+++ math_calculator.py
@@ -1,1 +1,17 @@
 import random 
+
+x = random.randint(0,100)
+y = random.randint(0,100)
+
+print(f'what is {x} + {y}?')
+
+answer = x + y
+
+response = int(input("what is the answer? "))
+
+if answer == response:
+    print('correct')
+else:
+    print('incorrect')
+
+
